f7228e7d05eef75573d580f6a1694551c81adf8b
ketanprasad9709 <158566774+ketanprasad9709@users.noreply.github.com>
2025-01-29 11:01:47 +0530

    Update login_signUp-context.js

--- src/context/login_signUp-context.js
+++ src/context/login_signUp-context.js
@@ -20,17 +20,18 @@ const initialState = {
     user_name: "",
     mobile_number: null,
     email_address: "",
-    user_ID: ""
+    user_ID: "",
+    hasShownLoginToast: false
 };
 
 const login_signUp = createContext(initialState);
 
 const LoginSignUpProvider = ({children}) => {
 
-    const [ { login, signUp, signUp_postData_status, signup_test, logout, login_signUp_modalStatus, mobileNumberLogin, mobileNumberSignUp, name, email, password, signup_password, signup__confirm_password, access_token, user_name, mobile_number, email_address, user_ID }, dispatchLogin_SignUp ]= useReducer(loginSignUpReducer, initialState );
+    const [ { login, signUp, signUp_postData_status, signup_test, logout, login_signUp_modalStatus, mobileNumberLogin, mobileNumberSignUp, name, email, password, signup_password, signup__confirm_password, access_token, user_name, mobile_number, email_address, user_ID, hasShownLoginToast }, dispatchLogin_SignUp ]= useReducer(loginSignUpReducer, initialState );
 
         return (
-            <login_signUp.Provider value={{ login, signUp, signUp_postData_status, signup_test, logout, login_signUp_modalStatus, mobileNumberLogin, mobileNumberSignUp, name, email, password, signup_password, signup__confirm_password, access_token, user_name, mobile_number, email_address, user_ID, dispatchLogin_SignUp }}>
+            <login_signUp.Provider value={{ login, signUp, signUp_postData_status, signup_test, logout, login_signUp_modalStatus, mobileNumberLogin, mobileNumberSignUp, name, email, password, signup_password, signup__confirm_password, access_token, user_name, mobile_number, email_address, user_ID, hasShownLoginToast, dispatchLogin_SignUp }}>
                 {children}
             </login_signUp.Provider>
         )
